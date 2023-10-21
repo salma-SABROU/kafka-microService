5f7c0ad431fd6964de4cc427a89b5fb756bf023b
salma-SABROU <129564311+salma-SABROU@users.noreply.github.com>
2023-10-21 21:21:20 +0100

    Add files via upload

---
+++ customer-service/src/main/java/com/sdia/customerservice/repo/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.sdia.customerservice.repo;
+
+import com.sdia.customerservice.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
---
+++ order-service/src/main/java/com/sdia/orderservice/enums/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.sdia.orderservice.enums;
+
+public enum OrderStatus {
+    CREATED, PENDING, DELIVERED, CANCELED
+}
